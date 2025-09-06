ca89bb0c7cdec872bd2391f18f1c311edc2f483c
hardik0832 <114808981+hardik0832@users.noreply.github.com>
2025-09-06 07:00:29 +0530

    Time: 0 ms (100%), Space: 7.9 MB (10.93%) - LeetHub

---
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string toHex(int num) {
+        if(!num) return"0";
+        unsigned int ourNum = num;
+        string res;
+        string hex ="0123456789abcdef";
+        while(ourNum){
+            res+= hex[ourNum % 16];
+            ourNum /= 16;
+
+        }
+        reverse(res.begin(), res.end());
+        return res;
+        
+    }
+};
